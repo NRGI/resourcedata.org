850589f83598ea4c6f5adb17dcee7c911a1ea49f
Matt Fullerton <matt.fullerton@gmail.com>
2017-06-16 21:57:56 +0200

    All steps for harvesting RGI documents

--- harvesters/rgi/scripts/run_harvester.sh
+++ harvesters/rgi/scripts/run_harvester.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+
 curl http://rgi.nrgi-assessment.org/api/public/assessments > assessment_list.json
 jq '.[] | .assessment_ID' ./assessment_list.json > assessments.csv
+python ./get_pdfs.py
+python ./rgi_import.py
